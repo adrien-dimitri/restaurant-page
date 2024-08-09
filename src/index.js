76dc7698eafa1b4ab9f83b195a27eafc7aec7476
Adrien <amaugueret@gmail.com>
2024-08-09 21:36:26 +0200

    added nav button functionality

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 import './style.css';
 import loadHome from './home.js';
 import createNav from './navigation.js';
+import loadMenu from './menu.js';
 
 createNav();
-
 loadHome();
+
+const buttons = document.querySelectorAll("nav button");
+
+buttons.forEach(button => {
+    button.addEventListener(("click"), () => {
+        document.querySelector("#content").innerHTML = ""
+        switch (button.dataset.action) {
+            case "home":
+                loadHome();
+                break;
+            case "menu":
+                loadMenu();
+                break;
+            case "about":
+                console.log("PRESSED ABOUT");
+                break;
+            case "order":
+                console.log("PRESSED ORDER");
+                break;
+        }
+    });
+});
+
+
