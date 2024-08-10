d235c6d7d70993e34d8a0f74afea272244e7c7d9
Adrien <amaugueret@gmail.com>
2024-08-10 03:03:26 +0200

    added fade in on load

--- src/home.js
+++ src/home.js
@@ -1,14 +1,20 @@
-
 export default function loadHome() {
     const RestName = document.createElement('div');
     const RestInfo = document.createElement('div');
 
     RestName.classList.add("restaurant-name");
     RestInfo.classList.add("order-online");
-    
+
     RestName.innerHTML = "PASTA HEAVEN";
     RestInfo.innerHTML = "ONLINE ORDERING NOW AVAILABLE!";
 
     document.querySelector("#content").appendChild(RestName);
     document.querySelector("#content").appendChild(RestInfo);
+
+    setTimeout(() => {
+        RestName.classList.add('loaded');
+    }, 0);
+    setTimeout(() => {
+        RestInfo.classList.add("loaded");
+    }, 1000)
 }
