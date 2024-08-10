96b1e4eb15dbf67ea87433202cbd3263e8cd4828
Adrien <amaugueret@gmail.com>
2024-08-11 00:28:08 +0200

    added image webpack loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,14 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [{
+          loader: 'image-webpack-loader',
+          options: {
+              pngquant: {
+                  quality: [.90, .95],
+              },
+          }
+        }],
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
